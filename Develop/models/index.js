0d960c5813ec9d375b81e5979bb8ce359d41c677
jdporr <jdporrazzo@gmail.com>
2024-04-09 18:04:36 -0400

    update model index.js and tag and product routes

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -7,6 +7,7 @@ const ProductTag = require('./ProductTag');
 // Products belongsTo Category
 Product.belongsTo(Category, {
   foreignKey: 'category_id',
+  onDelete: "cascade"
 });
 // Categories have many Products
 Category.hasMany(Product, {
@@ -14,16 +15,13 @@ Category.hasMany(Product, {
 })
 // Products belongToMany Tags (through ProductTag)
 Product.belongsToMany(Tag, {
-  through: {
-    model: ProductTag,
-    unique: false,
-  },
+  foreignKey: "product_id",
+  through: ProductTag
 });
 // Tags belongToMany Products (through ProductTag)
 Tag.belongsToMany(Product, {
-  through: {
-    model: ProductTag,
-  }
+  foreignKey: "tag_id",
+  through: ProductTag
 })
 
 module.exports = {
